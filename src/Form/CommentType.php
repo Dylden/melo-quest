5c3ee0df011599a1d6d4a0bb50daa81f7da9f9e7
Dylden <dylan.dennig@lapiscine.pro>
2024-12-28 17:19:31 +0100

    css comments

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,11 +17,11 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('comment')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
+            ->add('comment', TextareaType::class)
+            //->add('user', EntityType::class, [
+               // 'class' => User::class,
+              //  'choice_label' => 'id',
+           // ])
             //->add('track', EntityType::class, [
               //  'class' => Track::class,
                 //'choice_label' => 'id',
